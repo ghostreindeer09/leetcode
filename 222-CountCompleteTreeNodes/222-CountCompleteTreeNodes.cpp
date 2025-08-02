f718289b6d1fd6e8e60e7721c90859f06b72aa2b
Rishit Sharma <ghostreindeer009@gmail.com>
2025-08-02 22:46:44 +0530

    Create 222-CountCompleteTreeNodes

---
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/2/2025, 10:46:45 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+       if(root == NULL) return 0;
+
+        int left = countNodes(root->left);
+        int right = countNodes(root->right);
+
+        return 1+left+right;
+    }
+};
