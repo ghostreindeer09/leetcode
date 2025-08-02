af8424c789f82a14b2afc973ab81435a21cd9039
Rishit Sharma <ghostreindeer009@gmail.com>
2025-08-02 22:45:55 +0530

    Create 784-InsertIntoABinarySearchTree

---
+++ 784-InsertIntoABinarySearchTree/784-InsertIntoABinarySearchTree.cpp
@@ -0,0 +1,21 @@
+// Last updated: 8/2/2025, 10:45:56 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr) return new TreeNode(val);
+        if(val < root->val) root->left = insertIntoBST(root->left,val);
+        else root->right =  insertIntoBST(root->right,val);
+        return root;
+    }
+};
