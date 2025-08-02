514a01251e62e04eddb326232188396775364834
Rishit Sharma <ghostreindeer009@gmail.com>
2025-08-02 22:44:55 +0530

    Create 3747-MaximumDifferenceBetweenAdjacentElementsInACircularArray

---
+++ 3747-MaximumDifferenceBetweenAdjacentElementsInACircularArray/3747-MaximumDifferenceBetweenAdjacentElementsInACircularArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/2/2025, 10:44:56 PM
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int maxDiff = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            int next = (i+1)%n;
+            int diff = abs(nums[i]-nums[next]);
+            maxDiff = max(maxDiff,diff);
+        }
+        return maxDiff;
+    }
+};
