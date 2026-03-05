9a5298efaf4a92017c34b3cac8ca1ab788fe73bb
Rishit Sharma <ghostreindeer009@gmail.com>
2026-03-05 14:13:57 +0530

    Create 334-IncreasingTripletSubsequence

---
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.cpp
@@ -0,0 +1,19 @@
+// Last updated: 05/03/2026, 14:13:57
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+        for(int n:nums){
+            if(n <= min1){
+                min1 = n;
+            }
+            else if(n <= min2){
+                min2 = n;
+            }
+            else
+            return true;
+        }
+        return false;
+    }
+};
